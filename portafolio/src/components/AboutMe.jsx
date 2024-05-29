b6766eb418fbbb3b40ab1a9f647ec80d9b15199a
Bianca <cal22272@uvg.edu.gt>
2024-05-29 00:08:40 -0600

    fix: subtle changes

--- portafolio/src/components/AboutMe.jsx
+++ portafolio/src/components/AboutMe.jsx
@@ -9,7 +9,10 @@ function AboutMe() {
                     <div className="col-xs-12">
                         <h2>About Me</h2>
                         <hr className="section-hr" />
-                        <p>Soy estudiante de tercer año y estudio Ingeniería en Ciencias de la Computación y Tecnologías de la Información.</p>
+                        <p>Mi nombre es Bianca Calderón, tengo 20 años de edad y soy estudiante de Ingeniería en Ciencias de la Computación y Tecnologías de la Información en la Universidad del Valle de Guatemala. 
+                            Actualmente, estoy cursando mi tercer año de carrera. Me enfoco principalmente en la armonía del frontend, combinando colores y tipografías de manera adecuada.
+                            Junto a un grupo de compañeros, estamos desarrollando un proyecto que integra herramientas de backend, frontend y bases de datos. Puedes encontrar este y otros proyectos en mi portafolio.
+                        </p>
                     </div>
                 </div>
             </div>
