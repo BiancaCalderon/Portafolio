8d6485487d80b8a80a488d3d4ffdc2a9fbf7f561
Bianca <cal22272@uvg.edu.gt>
2024-05-28 22:56:49 -0600

    initial structure

---
+++ portafolio/src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './index.css';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import AboutMe from './components/AboutMe';
+import Portfolio from './components/Portfolio';
+import Skillset from './components/Skillset';
+import Experience from './components/Experience';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+
+function App() {
+    return (
+        <div>
+            <Header />
+            <Navbar />
+            <AboutMe />
+            <Portfolio />
+            <Skillset />
+            <Experience />
+            <Contact />
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
